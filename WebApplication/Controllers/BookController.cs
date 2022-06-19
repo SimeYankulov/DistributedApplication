2f852554666c5d0d407afb69243689826c872cab
SimeYankulov <83870858+SimeYankulov@users.noreply.github.com>
2022-06-19 12:24:00 +0200

    Add files via upload

---
+++ WebApplication/Controllers/BookController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationService.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.ViewModels;
+
+namespace WebApplication.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: BookController
+        public async Task<ActionResult> IndexAsync()
+        {
+            ServiceReference.Service1Client service = new ServiceReference.Service1Client();
+
+            List<BookVM> bookVMs = new List<BookVM>();
+
+            foreach (var item in await service.GetBooksAsync().ConfigureAwait(false))
+            {
+                bookVMs.Add(new BookVM(item));
+            }
+            return View(bookVMs);
+        }
+
+        // GET: BookController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BookController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BookController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BookVM bookVM)
+        {
+            ServiceReference.Service1Client service = new ServiceReference.Service1Client();
+            try
+            {
+                BookDTO bookDTO = new BookDTO
+                {
+                    ID = bookVM.Id,
+                    Name = bookVM.Name,
+                    Pages = bookVM.Pages,
+                    Author = bookVM.Author,
+                    Type = bookVM.Type
+                };
+                service.PostBookAsync(bookDTO);
+                return RedirectToAction("Index");
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BookController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            ServiceReference.Service1Client service = new ServiceReference.Service1Client();
+
+            BookVM bookVM = new BookVM();
+
+            var bookDto = service.GetBookByIDAsync(id);
+            bookVM = new BookVM(bookDto);
+
+            return View(bookVM);
+        }
+
+        // POST: BookController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(BookVM bookVM)
+        {
+            ServiceReference.Service1Client service = new ServiceReference.Service1Client();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    BookDTO bookDTO = new BookDTO
+                    {
+                        ID = bookVM.Id,
+                        Name = bookVM.Name,
+                        Pages = bookVM.Pages,
+                        Author = bookVM.Author,
+                        Type = bookVM.Type
+
+                    };
+                    service.PostBookAsync(bookDTO);
+                }
+                RedirectToAction("Index");
+                //ViewBag.Books = LoadDataUtilities.LoadStudentsData();
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BookController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            ServiceReference.Service1Client service = new ServiceReference.Service1Client();
+
+            BookVM bookVM = new BookVM();
+
+            service.DeleteBookAsync(id);
+
+            return RedirectToAction("Index");
+        }
+
+        // POST: BookController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(IndexAsync));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication/Controllers/BorrowController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationService.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.ViewModels;
+
+namespace WebApplication.Controllers
+{
+    public class BorrowController : Controller
+    {
+        // GET: BorrowController
+        public async Task<ActionResult> IndexAsync()
+        {
+            ServiceReference.Service1Client service = new ServiceReference.Service1Client();
+
+            List<BorrowVM> borrowVMs = new List<BorrowVM>();
+
+            foreach (var item in await service.GetBorrowsAsync().ConfigureAwait(false))
+            {
+                borrowVMs.Add(new BorrowVM(item));
+            }
+            return View(borrowVMs);
+        }
+
+        // GET: BorrowController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BorrowController/Create
+        public ActionResult Create()
+        {
+
+                return View();
+            
+        }
+
+        // POST: BorrowController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BorrowVM borrowVM)
+        {
+            ServiceReference.Service1Client service = new ServiceReference.Service1Client();
+            try
+            {
+                BorrowDTO borrowDTO = new BorrowDTO
+                {
+                    ID = borrowVM.ID,
+                    StudentID = borrowVM.stuID,
+                    BookID = borrowVM.bookID,
+                    DateBorrowed = borrowVM.DateBorrowed,
+                    DateReturned = borrowVM.DateReturned
+                };
+                service.PostBorrowAsync(borrowDTO);
+                return RedirectToAction("Index");
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BorrowController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            ServiceReference.Service1Client service = new ServiceReference.Service1Client();
+
+            BorrowVM borrowVM = new BorrowVM();
+
+            var borrowDto = service.GetBorrowByIDAsync(id);
+            borrowVM = new BorrowVM(borrowDto);
+
+            return View(borrowVM);
+        }
+
+        // POST: BorrowController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(BorrowVM borrowVM)
+        {
+            ServiceReference.Service1Client service = new ServiceReference.Service1Client();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    BorrowDTO borrowDTO = new BorrowDTO
+                    {
+                        ID = borrowVM.ID,
+                        StudentID = borrowVM.stuID,
+                        BookID = borrowVM.bookID,
+                        DateBorrowed = borrowVM.DateBorrowed,
+                        DateReturned = borrowVM.DateReturned
+
+                    };
+                    service.PostBorrowAsync(borrowDTO);
+                }
+                RedirectToAction("Index");
+                //ViewBag.Books = LoadDataUtilities.LoadStudentsData();
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BorrowController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            ServiceReference.Service1Client service = new ServiceReference.Service1Client();
+
+            BorrowVM borrowVM = new BorrowVM();
+
+            service.DeleteBorrowAsync(id);
+
+            return RedirectToAction("Index");
+        }
+
+        // POST: BorrowController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(IndexAsync));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
